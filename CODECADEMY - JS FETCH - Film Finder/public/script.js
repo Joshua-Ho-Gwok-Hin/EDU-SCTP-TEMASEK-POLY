b04fe847439da296e9aa4a4549646502c4ff5f94
Joshua Ho Gwok Hin <hgh.joshua@gmail.com>
2024-01-18 19:42:24 +0800

    Rename and Combine Codecademy

---
+++ CODECADEMY - JS FETCH - Film Finder/public/script.js
@@ -0,0 +1,68 @@
+const tmdbKey = 'a28ffb5de7b45d62fb1e82c16cde4472';
+const tmdbBaseUrl = 'https://api.themoviedb.org/3';
+const playBtn = document.getElementById('playBtn');
+
+const getGenres = async () => {
+  const genreRequestEndpoint = '/genre/movie/list';
+  const requestParams = `?api_key=${tmdbKey}`;
+  const urlToFetch = `${tmdbBaseUrl}${genreRequestEndpoint}${requestParams}`;
+  try {
+    const response = await fetch(urlToFetch);
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      return jsonResponse;
+    }
+  } catch (error) {
+    console.log(error);
+  };
+};
+
+const getMovies = async () => {
+  const selectedGenre = getSelectedGenre();
+  const discoverMovieEndpoint = '/discover/movie';
+  const requestParams = `?api_key=${tmdbKey}?with_genre=${selectedGenre}`;
+  const urlToFetch = `${tmdbBaseUrl}${discoverMovieEndpoint}${requestParams}`;
+  try {
+    const response = await fetch(urlToFetch);
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      const movies = jsonResponse.results;
+      console.log(movies);
+      return movies;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getMovieInfo = async (movie) => {
+  const movieId = movie.id;
+  const movieEndPoint = `/movie/${movieId}`;
+  const requestParams = `?api_key=${tmdbKey}`;
+  const urlToFetch = `${tmdbBaseUrl}${movieEndPoint}${requestParams}`;
+  try {
+    const response = await fetch(urlToFetch);
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      const movieInfo = jsonResponse;
+      return movieInfo;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Gets a list of movies and ultimately displays the info of a random movie from the list
+const showRandomMovie = async () => {
+  const movieInfo = document.getElementById('movieInfo');
+  if (movieInfo.childNodes.length > 0) {
+    clearCurrentMovie();
+  };
+
+  const movies = await getMovies();
+  const randomMovie = getRandomMovies(movies);
+  const info = await getMovieInfo(randomMovie);
+  displayMovie(info);
+};
+getGenres().then(populateGenreDropdown);
+playBtn.onclick = showRandomMovie;
---
+++ README.md
@@ -0,0 +1,13 @@
+### Codecademy JavaScript Article Links:
+
+[Currying in JavaScript](https://www.codecademy.com/courses/learn-intermediate-javascript/articles/javascript-currying)
+
+[Hoisting in JavaScript](https://www.codecademy.com/courses/learn-intermediate-javascript/articles/javascript-hoisting)
+
+[Concurrency Model and Event Loop in JavaScript](https://www.codecademy.com/courses/learn-intermediate-javascript/articles/javascript-concurrency-model-and-event-loop)
+
+[Intro to Memory Management in JavaScript](https://www.codecademy.com/courses/learn-intermediate-javascript/articles/javascript-intro-to-memory-management)
+
+[Debugging Memory Issues in JavaScript](https://www.codecademy.com/courses/learn-intermediate-javascript/articles/javascript-debugging-memory-issues)
+
+[Design Patterns in JavaScript](https://www.codecademy.com/courses/learn-intermediate-javascript/articles/javascript-design-patterns)
